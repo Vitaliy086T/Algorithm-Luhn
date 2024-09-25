c1034ad1a599cb0b043e8ab1b35ba296611280d6
Vitaliy <defaultrofler@gmail.com>
2024-09-25 19:03:39 +0300

    1st version

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void algorithm_luna(string s){
+    int sum = 0;
+    cout << "Your input card: " << s << endl; // Our example
+    for (int i = 0; i < s.length(); i++){
+        int temp = (s[i] - '0'); // char to int
+        if(i % 2 != 0){
+            sum += temp;
+        }
+        if(i % 2 == 0){
+            temp = temp * 2;
+            if(temp <= 9){
+                sum += temp;
+            }
+            if(temp > 9){
+                temp = temp - 9;
+                sum += temp;
+            }
+        }
+    }
+    if(sum%10 == 0){
+        cout << "This credit card " << s << " is correct by algorithm Luna\n";
+    }
+    else{
+        cout << "This credit card " << s << " is incorrect by algorithm Luna\n";
+    }
+}
+
+int main(){
+    string s = "5062821234567892"; //example temp
+    algorithm_luna(s);
+    return 0;
+}
